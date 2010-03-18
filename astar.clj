ededf091f5c059de371976215b50c0a07ba1bec1
Martin Biely <code@biely.eu>
2010-03-18 22:44:11 +0100

    fixed astar to compile

--- astar.clj
+++ astar.clj
@@ -1,10 +1,15 @@
-(ns astar
-  (require (dijkstra)))
+(ns astar)
 
+(defn astar [src nbrs cost heu dst]
+  (filter #(= dst %) (dijkstra/dijkstra-seq src nbrs 
+					    (fn [i j] (+ (cost i j)
+							 (heu j dst))))))
 
-(defn astar (defn dijkstra [src nbrs cost heu dst]
-  (filter #(= dst %) (dijkstra-seq src nbrs 
-				   (fn [i j] (+ (cost i j)
-						(heu j dst))))))
+(defmacro astar-path [& args]
+  "Like astar but fetches path"
+  `(:path (astar ~@args)))
+(defmacro astar-cost [& args]
+  "Like astar but fetches cost"
+  `(:cost (astar ~@args)))
 
 
