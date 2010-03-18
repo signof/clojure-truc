23698419dd36f5709ed84655622eb8b658ddfdc4
Martin Biely <code@biely.eu>
2010-03-18 22:36:07 +0100

    a-star search implemented

--- astar.clj
+++ astar.clj
@@ -1,1 +1,10 @@
+(ns astar
+  (require (dijkstra)))
+
+
+(defn astar (defn dijkstra [src nbrs cost heu dst]
+  (filter #(= dst %) (dijkstra-seq src nbrs 
+				   (fn [i j] (+ (cost i j)
+						(heu j dst))))))
+
 
